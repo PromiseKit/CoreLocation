182702521fecc19cc62f046f85e0e3684b3ba1e0
Max Howell <mxcl@me.com>
2016-09-12 17:31:03 -1000

    Fix tests with Xcode 8 GM

--- Tests/CLGeocoderTests.swift
+++ Tests/CLGeocoderTests.swift
@@ -6,7 +6,7 @@ import XCTest
 class CLGeocoderTests: XCTestCase {
     func test_reverseGeocodeLocation() {
         class MockGeocoder: CLGeocoder {
-            private override func reverseGeocodeLocation(_ location: CLLocation, completionHandler: CLGeocodeCompletionHandler) {
+            private override func reverseGeocodeLocation(_ location: CLLocation, completionHandler: @escaping CLGeocodeCompletionHandler) {
                 after(interval: 0).then {
                     completionHandler([dummyPlacemark], nil)
                 }
@@ -24,7 +24,7 @@ class CLGeocoderTests: XCTestCase {
     func test_geocodeAddressDictionary() {
         class MockGeocoder: CLGeocoder {
 
-            private override func geocodeAddressDictionary(_ addressDictionary: [AnyHashable : Any], completionHandler: CLGeocodeCompletionHandler) {
+            private override func geocodeAddressDictionary(_ addressDictionary: [AnyHashable : Any], completionHandler: @escaping CLGeocodeCompletionHandler) {
                 after(interval: 0.0).then {
                     completionHandler([dummyPlacemark], nil)
                 }
@@ -41,7 +41,7 @@ class CLGeocoderTests: XCTestCase {
 
     func test_geocodeAddressString() {
         class MockGeocoder: CLGeocoder {
-            override func geocodeAddressString(_ addressString: String, completionHandler: CLGeocodeCompletionHandler) {
+            override func geocodeAddressString(_ addressString: String, completionHandler: @escaping CLGeocodeCompletionHandler) {
                 after(interval: 0.0).then {
                     completionHandler([dummyPlacemark], nil)
                 }
